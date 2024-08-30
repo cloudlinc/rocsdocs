94e432b02092e6d986cc481c9a334626c6d1da54
Cloudlinc <96490617+cloudlinc@users.noreply.github.com>
2024-08-29 21:53:45 -0500

    fix speedinsights

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,10 +1,10 @@
-import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
  
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
-      <Analytics />
+      <SpeedInsights />
     </>
   );
 }
